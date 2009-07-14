36c0533efeeff1d33281ce37ebd040ed409ab5bd
alex <alex@bff75499-2b28-0410-8f74-d29e96b56331>
2009-07-14 19:34:20 +0000

    deleting wrong folder

--- sportrait-parent/tags/sportrait_web-1.6.2a/src/main/webapp/js/bookmark.js
+++
@@ -1,9 +0,0 @@
-function NewFavorite(){
-
-if(window.external){
-window.external.addFavorite(self.location, 'SPORTRAIT - Sportfotos online');
-}
-else{
-window.alert("To bookmark my site press CTRL + D");
-}
-}
