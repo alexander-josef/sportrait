1d8d5148fbf63ab2774183a10948ffbeccea8ccb
alex <alex@bff75499-2b28-0410-8f74-d29e96b56331>
2011-02-10 07:57:30 +0000

    setup ...

--- sportrait-parent/branches/1.6-branch/src/main/resources/schemas/add_on.sql
+++ sportrait-parent/branches/1.6-branch/src/main/resources/schemas/add_on.sql
@@ -4,12 +4,10 @@
 
 drop sequence sequence_genericlevelid;
 drop sequence sequence_userprofileid;
-drop sequence sequence_photographerid;
 drop sequence sequence_producttypeid;
 -- took out the dropping of the view-tables; separate script and ant task added to build script
 -- needed so that the manually inserted entries dont prohibit future insertions because the sequence starts with 1
 -- N O T E  that in PostgreSQL prior to version 7.4 the oracle syntax "create sequence xxx start with ..." does not work. hence this postgreSQL version of the script
 create sequence sequence_genericlevelid start 100;
 create sequence sequence_userprofileid start 100;
-create sequence sequence_photographerid start 100;
 create sequence sequence_producttypeid start 100;
