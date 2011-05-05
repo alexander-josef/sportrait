a9e305b3ae9f6b70e787f6eff6312782aca12b10
alex <alex@bff75499-2b28-0410-8f74-d29e96b56331>
2011-05-05 14:50:27 +0000

    Added image and check for valid downloads
    bugzId: 11113

--- sportrait-parent/branches/1.6-branch/src/main/java/ch/unartig/studioserver/actions/DownloadPhotoAction.java
+++ sportrait-parent/branches/1.6-branch/src/main/java/ch/unartig/studioserver/actions/DownloadPhotoAction.java
@@ -21,6 +21,15 @@ public class DownloadPhotoAction extends Action {
 
     Logger _logger = Logger.getLogger(getClass().getName());
 
+    /**
+     * Send back high res foto via http servlet repsonse. Check if album of photo has free digital option
+     * @param actionMapping
+     * @param actionForm
+     * @param request
+     * @param httpServletResponse
+     * @return
+     * @throws UnartigException
+     */
     public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse httpServletResponse) throws UnartigException {
 
         String photoId = request.getParameter("photoId");
@@ -28,22 +37,24 @@ public class DownloadPhotoAction extends Action {
         Photo photo = photoDAO.load(Long.valueOf(photoId));
 
         // todo check if photo is allowed to be downloaded
-        _logger.debug("photo [" + photo.getFilename() + "] will be downloaded");
-        try {
-            // use application/octet-stream instead to download and not display the image??
-            httpServletResponse.setContentType("image/JPG");
-            httpServletResponse.setHeader("Content-Disposition", "attachment; filename=\"" + photo.getFilename() + "\"");
-            // What's the content length? set content-length header:
-            httpServletResponse.setContentLength((int)photo.getFile().length());
-            // copy the file to the outputstream:
-            // digital negativ copy the file to the output stream
-            _logger.info("streaming the digital negativ");
-            FileUtils.copyFile(photo.getFile(), httpServletResponse.getOutputStream());
-        } catch (IOException e) {
-            // for example if high-res file was not found ...
-            throw new UnartigException("cannot stream photo for download ...", e);
-        }
 
+        if (photo.getAlbum().isHasFreeHighResDownload()) {
+            _logger.debug("photo [" + photo.getFilename() + "] will be downloaded");
+            try {
+                // use application/octet-stream instead to download and not display the image??
+                httpServletResponse.setContentType("image/JPG");
+                httpServletResponse.setHeader("Content-Disposition", "attachment; filename=\"" + photo.getFilename() + "\"");
+                // What's the content length? set content-length header:
+                httpServletResponse.setContentLength((int) photo.getFile().length());
+                // copy the file to the outputstream:
+                // digital negativ copy the file to the output stream
+                _logger.info("streaming the digital negativ");
+                FileUtils.copyFile(photo.getFile(), httpServletResponse.getOutputStream());
+            } catch (IOException e) {
+                // for example if high-res file was not found ...
+                throw new UnartigException("cannot stream photo for download ...", e);
+            }
+        }
         // todo error page if photo not available?
         // todo possible option to download different version (smaller resolution) of the file?
         return null;
