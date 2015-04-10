af5e51c72ea2d6f726cff05698d6bc1e420e2cb7
alex <alex@bff75499-2b28-0410-8f74-d29e96b56331>
2015-04-10 17:06:47 +0000

    [maven-release-plugin]  copy for tag sportrait_web-1.6.15

---
+++ sportrait-parent/tags/sportrait_web-1.6.15/src/main/java/ch/unartig/sportrait/reports/IReportColumn.java
@@ -0,0 +1,29 @@
+/*-*
+ *
+ * FILENAME  :
+ *
+ *    @author alexanderjosef
+ *    @since Nov 19, 2007
+ *
+ * Copyright (c) 2007 Alexander Josef,unartig AG; All rights reserved
+ *
+ * STATUS  :
+ *    $Revision: $REVISION$
+ *
+ *    $Author: $AUTHOR$ $, $Locker$
+ *
+ *************************************************
+ * $Log$
+ *
+ ****************************************************************/
+package ch.unartig.sportrait.reports;
+
+/**
+ * Represents the column headers for a report
+ */
+public interface IReportColumn
+{
+    int getMonth();
+
+    int getYear();
+}
