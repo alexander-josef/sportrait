f7120deaf1f14a8af281ff928d04fc7ebbfeb0ae
ajosef <ajosef@bff75499-2b28-0410-8f74-d29e96b56331>
2015-07-09 20:34:13 +0000

    [maven-release-plugin]  copy for tag sportrait_web-1.6.32

---
+++ sportrait-parent/tags/sportrait_web-1.6.32/src/main/java/ch/unartig/studioserver/persistence/DAOs/UserRoleDAO.java
@@ -0,0 +1,43 @@
+/*-*
+ *
+ * FILENAME  :
+ *    $RCSfile$
+ *
+ *    @author alex$
+ *    @since 07.06.2007$
+ *
+ * Copyright (c) 2006 Alexander Josef,unartig AG; All rights reserved
+ *
+ * STATUS  :
+ *    $Revision$, $State$, $Name$
+ *
+ *    $Author$, $Locker$
+ *    $Date$
+ *
+ *************************************************
+ * $Log$
+ * Revision 1.1  2007/06/09 11:15:37  alex
+ * photographer
+ *
+ ****************************************************************/
+package ch.unartig.studioserver.persistence.DAOs;
+
+import ch.unartig.exceptions.UAPersistenceException;
+import ch.unartig.studioserver.model.UserRole;
+import ch.unartig.studioserver.persistence.util.HibernateUtil;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+
+public class UserRoleDAO
+{
+
+    public UserRole loadRoleByName(String adminRoleName) throws UAPersistenceException
+    {
+        Criteria c = HibernateUtil.currentSession()
+                .createCriteria(UserRole.class)
+                .add(Restrictions.eq("roleName", adminRoleName));
+        return (UserRole)c.uniqueResult();
+    }
+
+    
+}
---
+++ sportrait-parent/tags/sportrait_web-1.6.32/src/main/webapp/js/GhostBoxes.js
@@ -0,0 +1,25 @@
+function showWindow(id)
+{
+	document.getElementById(id).style.visibility="visible";
+}
+
+function hideWindow(id)
+{
+	document.getElementById(id).style.visibility="hidden";
+}
+
+function windowShow(id)
+{
+	document.getElementById(id).style.display="block";
+}
+
+function windowHide(id)
+{
+	document.getElementById(id).style.display="none";
+}
+
+function readOnly(id)
+{
+	document.getElementById(id).style.readonly="readonly";
+}
+
