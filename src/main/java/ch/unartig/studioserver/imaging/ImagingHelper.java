aca47e308020ca452679d6db70ac6384223348d9
ajosef <22647518+alexander-josef@users.noreply.github.com>
2016-03-06 16:40:50 +0000

    parameter for sponsor bar / logo fix problem with build process for "test" profile

--- src/main/java/ch/unartig/studioserver/imaging/ImagingHelper.java
+++ src/main/java/ch/unartig/studioserver/imaging/ImagingHelper.java
@@ -210,8 +210,13 @@ public class ImagingHelper
      * @throws IOException
      */
     private static BufferedImage getLogoImage() throws IOException {
+        // todo parameter
         _logger.debug("Trying to read Sola logo png image from file system ... ");
-        return ImageIO.read(new FileInputStream("/Users/alexanderjosef/Pictures/work-folder-sportrait-images/sponsoren-logo-overlay/sola-logo-upper-left.png"));
+        String filename = "/Users/alexanderjosef/Pictures/work-folder-sportrait-images/sponsoren-logo-overlay/sola-logo-upper-left.png";
+
+        _logger.debug("Read logo image for montage : " + Registry.getLogoImageFile());
+
+        return ImageIO.read(new FileInputStream(filename));
     }
 
 
@@ -221,8 +226,13 @@ public class ImagingHelper
      * @throws IOException
      */
     private static BufferedImage getSponsorBar() throws IOException {
+        // todo parameter
         _logger.debug("Trying to read sola sponsor bar png image from file system ... ");
-        return ImageIO.read(new FileInputStream("/Users/alexanderjosef/Pictures/work-folder-sportrait-images/sponsoren-logo-overlay/sola-sponsors-bar-bottom.png"));
+        String filename = "/Users/alexanderjosef/Pictures/work-folder-sportrait-images/sponsoren-logo-overlay/sola-sponsors-bar-bottom.png";
+
+        _logger.debug("Read sponsor bar for montage : " + Registry.getSponsorBarFile());
+
+        return ImageIO.read(new FileInputStream(filename));
     }
 
 
