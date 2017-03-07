15ba6bf4b58640bfae4702b756c4f82bc466784e
ajosef <ajosef@bff75499-2b28-0410-8f74-d29e96b56331>
2017-03-07 21:51:34 +0000

    Added new request processor to protect actions with roles. roles not yet configured for all actions that need to be protected. DO NOT USE IN PRODUCTION.

---
+++ sportrait-parent/branches/1.6-branch/src/main/java/ch/unartig/controller/UnartigCustomSecurityRequestProcessor.java
@@ -0,0 +1,50 @@
+package ch.unartig.controller;
+
+import ch.unartig.studioserver.Registry;
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.SecureTilesRequestProcessor;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by alexanderjosef on 07.03.17.
+ */
+public class UnartigCustomSecurityRequestProcessor extends SecureTilesRequestProcessor {
+
+    Logger logger = Logger.getLogger(getClass().getName());
+
+    protected boolean processRoles(HttpServletRequest request,
+                                   HttpServletResponse response, ActionMapping mapping)
+            throws IOException, ServletException
+    {
+        // Is this action protected by role requirements?
+        String roles[] = mapping.getRoleNames();
+        if ((roles == null) || (roles.length < 1)) {
+            return (true);
+        }
+
+        // Check the current user against the list of required roles
+        HttpSession session = request.getSession();
+        Client client = (Client) session.getAttribute(Registry._SESSION_CLIENT_NAME);
+        if (client == null) {
+            response.sendError(403,"Please log in");
+            return false;
+        }
+        for (String role : roles) {
+            if (client.getAuthorizedRoleNames().contains(role)) {
+                logger.info("found logged in user with role \""+role+"\"");
+                return (true);
+            }
+        }
+        logger.info("No user found with role \"" + roles.toString() +"\" - Returning Not Authorized (403) Code");
+        response.sendError(403,"Please log in");
+//        response.sendError(HttpServletResponse.SC_BAD_REQUEST,getInternal().getMessage("notAuthorized",mapping.getPath()));
+        return (false);
+    }
+}
+
