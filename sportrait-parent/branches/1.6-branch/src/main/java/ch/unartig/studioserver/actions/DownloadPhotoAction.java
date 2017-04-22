bd3a391d9cbd38042e72ee8a9b8c9ed9ed9fa52d
ajosef <ajosef@bff75499-2b28-0410-8f74-d29e96b56331>
2017-04-22 21:34:33 +0000

    set logging in test to tomcat7, typos, logging statements

--- sportrait-parent/branches/1.6-branch/src/main/java/ch/unartig/studioserver/actions/DownloadPhotoAction.java
+++ sportrait-parent/branches/1.6-branch/src/main/java/ch/unartig/studioserver/actions/DownloadPhotoAction.java
@@ -32,7 +32,9 @@ public class DownloadPhotoAction extends Action {
      */
     public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse httpServletResponse) throws UnartigException {
 
+
         String photoId = request.getParameter("photoId");
+        _logger.debug("preparing download of photo with photoID " + photoId);
         PhotoDAO photoDAO = new PhotoDAO();
         Photo photo = photoDAO.load(Long.valueOf(photoId));
 
@@ -48,14 +50,19 @@ public class DownloadPhotoAction extends Action {
                 // todo: if needed, add a method in Photo do receive content length.
                 // httpServletResponse.setContentLength((int) photo.getFileContent().length());
                 // copy the file to the outputstream:
-                // digital negativ copy the file to the output stream
-                _logger.info("streaming the digital negativ");
+                // digital negative copy the file to the output stream
+                _logger.info("streaming the digital negative");
                 FileUtils.copyFile(photo.getFileContent(), httpServletResponse.getOutputStream());
             } catch (IOException e) {
                 // for example if high-res file was not found ...
                 throw new UnartigException("cannot stream photo for download ...", e);
             }
+        } else {
+            _logger.debug("no free download available for photo [" + photo.getFilename() + "]");
         }
+
+
+
         // todo error page if photo not available?
         // todo possible option to download different version (smaller resolution) of the file?
         return null;
