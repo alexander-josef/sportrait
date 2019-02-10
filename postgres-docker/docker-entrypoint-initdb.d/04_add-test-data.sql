d74f3882043ddfb9d23222b83797afdc05852d09
Alexander Josef <alexander.josef@netcetera.com>
2019-02-10 09:11:49 +0100

    adding postgres docker configuration to parent sportrait repo folder

---
+++ postgres-docker/docker-entrypoint-initdb.d/04_add-test-data.sql
@@ -0,0 +1,12 @@
+--
+-- PostgreSQL sportrait database docker container : initial test data setup
+--
+
+-- to be run after step 03 - initial setup stub data creation (admin users, products and necessary mappings, etc.)
+
+
+-- use db "sportrait" with user "sportrait"
+
+\c sportrait sportrait
+
+
