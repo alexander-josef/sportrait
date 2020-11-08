3d39a393447ae7cb020aa7ef8c94f64b9a7f7d55
Alexander Josef <alexander.josef@netcetera.com>
2020-11-08 14:38:44 +0100

    renaming api classes

--- src/main/java/com/sportrait/importrs/service/EventCategoriesApi.java
+++ src/main/java/com/sportrait/importrs/service/EventCategoriesApi.java
@@ -8,14 +8,22 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 // class for the eventCategories API - all verbs implemented below
-@Path("/eventCategories/{eventCategoryId}/albums")
+@Path("/eventCategories/")
 public class EventCategoriesApi {
     private final Logger _logger = Logger.getLogger(getClass().getName());
 
+    /**
+     * This API method will create / update an album for an eventCategory.
+     * NOTE: the current implementation only uses 1 album per eventCategory!
+     * @param eventCategoryId
+     * @param album
+     * @return
+     */
+    @Path("/{eventCategoryId}/albums")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public Response createAlbum(@PathParam("eventCategoryId") int eventCategoryId, Album album){
-        _logger.info("POSt /api/import/eventCategories/{eventCategoryId}/albums");
+        _logger.info("POST /api/import/eventCategories/{eventCategoryId}/albums");
         _logger.info("for eventCategoryId : " + eventCategoryId);
         _logger.debug(album);
 
@@ -24,19 +32,29 @@ public class EventCategoriesApi {
         return  Response.ok().entity(album).build();
     }
 
+    @Path("/{eventCategoryId}/albums")
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     public Response updateAlbum(@PathParam("eventCategoryId") int eventCategoryId, Album album){
 
+        if (album.getId()==null) {
+            _logger.info("PUT /api/import/eventCategories/{eventCategoryId}/albums");
+            _logger.info("album ID is null -> create new album");
+        } else {
+            _logger.info("POST /api/import/eventCategories/{eventCategoryId}/albums");
+            _logger.info("album ID = :"+album.getId() +" -> update album / import for given album");
+        }
 
         return  Response.ok().entity("not implemented").build();
     }
 
+    @Path("/{eventCategoryId}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAlbums(@PathParam("eventCategoryId") int eventCategoryId, Album album){
+    public Response getAlbums(@PathParam("eventCategoryId") int eventCategoryId){
 
 
+        // load event category
         return  Response.ok().entity("not implemented").build();
     }
 
