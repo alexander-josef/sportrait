b7bcbeab37603a620a2897fcaeed22c04dad9983
Alexander Josef <alexander.josef@netcetera.com>
2020-11-14 13:34:05 +0100

    adding listener for Jersey initialization call Registry.init() after Jersey has initialized

--- src/main/java/com/sportrait/importrs/ImportRestApplication.java
+++ src/main/java/com/sportrait/importrs/ImportRestApplication.java
@@ -1,9 +1,39 @@
 package com.sportrait.importrs;
 
+import ch.unartig.studioserver.Registry;
+import org.glassfish.jersey.server.monitoring.ApplicationEvent;
+import org.glassfish.jersey.server.monitoring.ApplicationEventListener;
+import org.glassfish.jersey.server.monitoring.RequestEvent;
+import org.glassfish.jersey.server.monitoring.RequestEventListener;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import static org.glassfish.jersey.server.monitoring.ApplicationEvent.Type.INITIALIZATION_FINISHED;
 
 @ApplicationPath("/api/import")
-public class ImportRestApplication extends Application {
-    // this initialises Jersey - nothing to do here
+public class ImportRestApplication extends Application implements ApplicationEventListener {
+
+    @Override
+    public void onEvent(ApplicationEvent applicationEvent) {
+
+        // this gets called after Jersey has finished initialisation //  good place for init methods?
+        if (applicationEvent.getType() == INITIALIZATION_FINISHED) {
+            try {
+                Registry.init();
+                System.out.println("***** Registry.init() from Jersey API Import App ******");
+            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | GeneralSecurityException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    @Override
+    public RequestEventListener onRequest(RequestEvent requestEvent) {
+        return null;
+    }
 }
